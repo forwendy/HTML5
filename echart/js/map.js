a9c95fee666db5dec8393351e0ef4180a8b734ec
wendy <402748215@qq.com>
2018-06-30 18:06:30 +0800

    add map style

---
+++ echart/js/map.js
@@ -0,0 +1,293 @@
+var data =[{"list":[{"address":"天津市西青区海泰大道与华科五路交叉口","id":"d3e4f37c-ab74-4d45-9ee3-e991457aab67","name":"OVU创客星天津站","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"0d489bf2-e40f-4afc-8725-cb2b094d8e16","spaceos_city_name":"天津","totalSpaceCount":588,"x_y":"117.083338,39.10733"}],"name":"天津","spaceos_city_id":"0d489bf2-e40f-4afc-8725-cb2b094d8e16","value":1},{"list":[{"address":"黄石经济技术开发区金山大道185号光谷联合科技城展示中心","id":"471c725392d54f0793be02588a143767","name":"黄石站","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"12","spaceos_city_name":"黄石","totalSpaceCount":54,"x_y":"115.008918,30.160115"}],"name":"黄石","spaceos_city_id":"12","value":1},{"list":[{"address":"西安市未央区ovu创客星","id":"3c1b812a12464a40911e263ba2b49c60","name":"西安站","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"1726cc7b771445a6bdc9b554ad01c192","spaceos_city_name":"西安","totalSpaceCount":500,"x_y":"108.859276,34.372401"}],"name":"西安","spaceos_city_id":"1726cc7b771445a6bdc9b554ad01c192","value":1},{"list":[{"address":"东营市东营区金融港","id":"6469a432-45ff-4d67-9f7d-e8114c3701b8","name":"OVU创客星东营站","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"20739cac-88d4-4286-ac04-d91036b294c8","spaceos_city_name":"东营","totalSpaceCount":3,"x_y":"118.710579,37.442364"}],"name":"东营","spaceos_city_id":"20739cac-88d4-4286-ac04-d91036b294c8","value":1},{"list":[{"address":"合肥市滨湖新区徽州大道与南京路交口金融港A5-A6","id":"35da7c09f4c94e05bdcdbe00d1cb2f35","name":"合肥金融港站","officeRentRate":"29.41%","office_space_type":"0","openOfficeRentRate":"72.50%","spaceos_city_id":"28425e8754854d7da8ce84dca2e3b44d","spaceos_city_name":"合肥","totalSpaceCount":238,"x_y":"31.7517340000,117.2965050000"}],"name":"合肥","spaceos_city_id":"28425e8754854d7da8ce84dca2e3b44d","value":1},{"list":[{"address":"山东省青岛市黄岛区峨眉山路396号光谷国际海洋信息港5号楼","id":"2f2d092e234f47079ad7c448d17a48af","name":"青岛站","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"6744e459616d4e7281729e062652fec3","spaceos_city_name":"青岛","totalSpaceCount":200,"x_y":"120.166868,35.957397"}],"name":"青岛","spaceos_city_id":"6744e459616d4e7281729e062652fec3","value":1},{"list":[{"address":"鄂州市华容区光谷联合科技城","id":"f342aa8ecdf24dd283fe9fe83295e21c","name":"创客星鄂州站","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"6ffaa9e443d94e828c68a98f82febe9d","spaceos_city_name":"鄂州","totalSpaceCount":31,"x_y":"114.663531,30.506431"}],"name":"鄂州","spaceos_city_id":"6ffaa9e443d94e828c68a98f82febe9d","value":1},{"list":[{"address":"湖北省武汉市东湖高新技术开发区关山大道软件园","id":"1ab811e4cd6f47f9914bd8e60dc335ee","name":"光谷软件园站","officeRentRate":"100.00%","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":235,"x_y":"114.414795,30.481601"},{"address":"湖北省武汉市洪山区野芷湖西路16号创意天地","id":"20151102134610","name":"创意天地南站","officeRentRate":"94.59%","office_space_type":"0","openOfficeRentRate":"91.67%","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":801,"x_y":"114.330074,30.474672"},{"address":"湖北省武汉市洪山区文治街17号","id":"20151118094521","name":"书城路站","officeRentRate":"94.29%","office_space_type":"0","openOfficeRentRate":"60.36%","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":617,"x_y":"114.345303,30.507134"},{"address":"湖北省武汉市江夏经济开发区藏龙岛27号企业总部基地一期2栋","id":"20151119203012","name":"研创中心站","officeRentRate":"92.86%","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":225,"x_y":"114.430782,30.42879"},{"address":"湖北省武汉市东湖高新技术开发区光谷金融港","id":"5171ffe7eea84f07b78ab39b6029ec89","name":"光谷金融港站","officeRentRate":"83.87%","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":403,"x_y":"114.427927,30.461822"},{"address":"武汉市江夏区光谷金融港-b14栋","id":"57b71c19-c25a-49c9-ade3-2203ef95d2ea","name":"金融港东站","officeRentRate":"13.24%","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":2452,"x_y":"114.432052,30.459704"},{"address":"武汉市东湖新技术开发区关南一路20号当代科技园5号楼","id":"80ffdaee-3228-49d8-bd09-af5b0f634439","name":"梦工场","officeRentRate":"91.67%","office_space_type":"0","openOfficeRentRate":"44.12%","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":888,"x_y":"114.423843,30.472361"},{"address":"湖北省武汉市武昌区丁字桥路9号","id":"9b70e593-8cfb-4083-bc47-a1077f01b3c0","name":"理想国际站","officeRentRate":"93.33%","office_space_type":"0","openOfficeRentRate":"96.61%","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":378,"x_y":"114.333519,30.52891"},{"address":"武汉市江岸区华中图书交易中心-北门","id":"b174c2e4-6f25-4c58-ab8e-12a68591c184","name":"出版城站","officeRentRate":"0.00%","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":3186,"x_y":"114.283924,30.633756"},{"address":"武汉市江岸区青岛路/洞庭街(路口)","id":"b1dcfc48-59ef-49fe-a4c6-f55dbf96981d","name":"OVUWORK","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":242,"x_y":"114.304732,30.587398"},{"address":"湖北省武汉市洪山区野芷湖西路16号创意天地","id":"b9988c948a1c43c0b83e53abdb71ca71","name":"创意天地北站","officeRentRate":"96.15%","office_space_type":"0","openOfficeRentRate":"63.82%","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":1498,"x_y":"114.3295,30.47787"},{"address":"湖北省武汉市江夏区光谷大道90号","id":"368b5433-bfc5-43db-b6a0-2934262e41b3","name":"金融港人才公寓","office_space_type":"1","sohoHouseTypeCount":1,"sohoRentRate0":"0.00%","sohoRentRateName0":"大单间","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":208,"x_y":"114.426624,30.461763"},{"address":"湖北省武汉市洪山区野芷湖西路16号创意天地","id":"8eb4e989ed634e7abb6c0a56d8d1d374","name":"创客星SOHO","office_space_type":"1","sohoHouseTypeCount":4,"sohoRentRate0":"100.00%","sohoRentRate1":"100.00%","sohoRentRate2":"93.48%","sohoRentRate3":"54.55%","sohoRentRateName0":"豪华办公单间","sohoRentRateName1":"奢华办公套间","sohoRentRateName2":"个性办公单间","sohoRentRateName3":"舒适办公两人间","spaceos_city_id":"7","spaceos_city_name":"武汉","totalSpaceCount":0,"x_y":"114.329356,30.474532"}],"name":"武汉","spaceos_city_id":"7","value":13},{"list":[{"address":"河南省信阳市高新区工三路企业服务广场G1号楼","id":"07c832ad-1b75-41f1-ae18-04e323e3b9f2","name":"信阳站","officeRentRate":"暂无数据","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"de6cfe75-0dd9-44d7-a538-efe4d784621b","spaceos_city_name":"信阳","totalSpaceCount":400,"x_y":","}],"name":"信阳","spaceos_city_id":"de6cfe75-0dd9-44d7-a538-efe4d784621b","value":1},{"list":[{"address":"北海市北海工业园中国电子北海产业园","id":"2eb9ae79-b6b6-4f5e-a02d-494862736bd1","name":"北海电子产业园","officeRentRate":"0.00%","office_space_type":"0","openOfficeRentRate":"暂无数据","spaceos_city_id":"fa4777d3e4244063a714c4425ebd858d","spaceos_city_name":"北海","totalSpaceCount":442,"x_y":"109.195085,21.545513"},{"address":"广西壮族自治区北海市海城区北部湾信息港","id":"636fa4da71a142869a3bae7118ca50a3","name":"北部湾信息港","officeRentRate":"0.00%","office_space_type":"0","openOfficeRentRate":"0.00%","spaceos_city_id":"fa4777d3e4244063a714c4425ebd858d","spaceos_city_name":"北海","totalSpaceCount":1631,"x_y":"109.197191,21.517516"}],"name":"北海","spaceos_city_id":"fa4777d3e4244063a714c4425ebd858d","value":2}];
+
+var geoCoordMap = {
+            '海门': [121.15, 31.89],
+            '鄂尔多斯': [109.781327, 39.608266],
+            '招远': [120.38, 37.35],
+            '舟山': [122.207216, 29.985295],
+            '齐齐哈尔': [123.97, 47.33],
+            '盐城': [120.13, 33.38],
+            '赤峰': [118.87, 42.28],
+            '青岛': [120.33, 36.07],
+            '乳山': [121.52, 36.89],
+            '金昌': [102.188043, 38.520089],
+            '泉州': [118.58, 24.93],
+            '莱西': [120.53, 36.86],
+            '日照': [119.46, 35.42],
+            '胶南': [119.97, 35.88],
+            '南通': [121.05, 32.08],
+            '拉萨': [91.11, 29.97],
+            '云浮': [112.02, 22.93],
+            '梅州': [116.1, 24.55],
+            '文登': [122.05, 37.2],
+            '上海': [121.48, 31.22],
+            '攀枝花': [101.718637, 26.582347],
+            '威海': [122.1, 37.5],
+            '承德': [117.93, 40.97],
+            '厦门': [118.1, 24.46],
+            '汕尾': [115.375279, 22.786211],
+            '潮州': [116.63, 23.68],
+            '丹东': [124.37, 40.13],
+            '太仓': [121.1, 31.45],
+            '曲靖': [103.79, 25.51],
+            '烟台': [121.39, 37.52],
+            '福州': [119.3, 26.08],
+            '瓦房店': [121.979603, 39.627114],
+            '即墨': [120.45, 36.38],
+            '抚顺': [123.97, 41.97],
+            '玉溪': [102.52, 24.35],
+            '张家口': [114.87, 40.82],
+            '阳泉': [113.57, 37.85],
+            '莱州': [119.942327, 37.177017],
+            '湖州': [120.1, 30.86],
+            '汕头': [116.69, 23.39],
+            '昆山': [120.95, 31.39],
+            '宁波': [121.56, 29.86],
+            '湛江': [110.359377, 21.270708],
+            '揭阳': [116.35, 23.55],
+            '荣成': [122.41, 37.16],
+            '连云港': [119.16, 34.59],
+            '葫芦岛': [120.836932, 40.711052],
+            '常熟': [120.74, 31.64],
+            '东莞': [113.75, 23.04],
+            '河源': [114.68, 23.73],
+            '淮安': [119.15, 33.5],
+            '泰州': [119.9, 32.49],
+            '南宁': [108.33, 22.84],
+            '营口': [122.18, 40.65],
+            '惠州': [114.4, 23.09],
+            '江阴': [120.26, 31.91],
+            '蓬莱': [120.75, 37.8],
+            '韶关': [113.62, 24.84],
+            '嘉峪关': [98.289152, 39.77313],
+            '广州': [113.23, 23.16],
+            '延安': [109.47, 36.6],
+            '太原': [112.53, 37.87],
+            '清远': [113.01, 23.7],
+            '中山': [113.38, 22.52],
+            '昆明': [102.73, 25.04],
+            '寿光': [118.73, 36.86],
+            '盘锦': [122.070714, 41.119997],
+            '长治': [113.08, 36.18],
+            '深圳': [114.07, 22.62],
+            '珠海': [113.52, 22.3],
+            '宿迁': [118.3, 33.96],
+            '咸阳': [108.72, 34.36],
+            '铜川': [109.11, 35.09],
+            '平度': [119.97, 36.77],
+            '佛山': [113.11, 23.05],
+            '海口': [110.35, 20.02],
+            '江门': [113.06, 22.61],
+            '章丘': [117.53, 36.72],
+            '肇庆': [112.44, 23.05],
+            '大连': [121.62, 38.92],
+            '临汾': [111.5, 36.08],
+            '吴江': [120.63, 31.16],
+            '石嘴山': [106.39, 39.04],
+            '沈阳': [123.38, 41.8],
+            '苏州': [120.62, 31.32],
+            '茂名': [110.88, 21.68],
+            '嘉兴': [120.76, 30.77],
+            '长春': [125.35, 43.88],
+            '胶州': [120.03336, 36.264622],
+            '银川': [106.27, 38.47],
+            '张家港': [120.555821, 31.875428],
+            '三门峡': [111.19, 34.76],
+            '锦州': [121.15, 41.13],
+            '南昌': [115.89, 28.68],
+            '柳州': [109.4, 24.33],
+            '三亚': [109.511909, 18.252847],
+            '自贡': [104.778442, 29.33903],
+            '吉林': [126.57, 43.87],
+            '阳江': [111.95, 21.85],
+            '泸州': [105.39, 28.91],
+            '西宁': [101.74, 36.56],
+            '宜宾': [104.56, 29.77],
+            '呼和浩特': [111.65, 40.82],
+            '成都': [104.06, 30.67],
+            '大同': [113.3, 40.12],
+            '镇江': [119.44, 32.2],
+            '桂林': [110.28, 25.29],
+            '张家界': [110.479191, 29.117096],
+            '宜兴': [119.82, 31.36],
+            '北海': [109.12, 21.49],
+            '西安': [108.95, 34.27],
+            '金坛': [119.56, 31.74],
+            '东营': [118.49, 37.46],
+            '牡丹江': [129.58, 44.6],
+            '遵义': [106.9, 27.7],
+            '绍兴': [120.58, 30.01],
+            '扬州': [119.42, 32.39],
+            '常州': [119.95, 31.79],
+            '潍坊': [119.1, 36.62],
+            '重庆': [106.54, 29.59],
+            '台州': [121.420757, 28.656386],
+            '南京': [118.78, 32.04],
+            '滨州': [118.03, 37.36],
+            '贵阳': [106.71, 26.57],
+            '无锡': [120.29, 31.59],
+            '本溪': [123.73, 41.3],
+            '克拉玛依': [84.77, 45.59],
+            '渭南': [109.5, 34.52],
+            '马鞍山': [118.48, 31.56],
+            '宝鸡': [107.15, 34.38],
+            '焦作': [113.21, 35.24],
+            '句容': [119.16, 31.95],
+            '北京': [116.46, 39.92],
+            '徐州': [117.2, 34.26],
+            '衡水': [115.72, 37.72],
+            '包头': [110, 40.58],
+            '绵阳': [104.73, 31.48],
+            '乌鲁木齐': [87.68, 43.77],
+            '枣庄': [117.57, 34.86],
+            '杭州': [120.19, 30.26],
+            '淄博': [118.05, 36.78],
+            '鞍山': [122.85, 41.12],
+            '溧阳': [119.48, 31.43],
+            '库尔勒': [86.06, 41.68],
+            '安阳': [114.35, 36.1],
+            '开封': [114.35, 34.79],
+            '济南': [117, 36.65],
+            '德阳': [104.37, 31.13],
+            '温州': [120.65, 28.01],
+            '九江': [115.97, 29.71],
+            '邯郸': [114.47, 36.6],
+            '临安': [119.72, 30.23],
+            '兰州': [103.73, 36.03],
+            '沧州': [116.83, 38.33],
+            '临沂': [118.35, 35.05],
+            '南充': [106.110698, 30.837793],
+            '天津': [117.2, 39.13],
+            '富阳': [119.95, 30.07],
+            '泰安': [117.13, 36.18],
+            '诸暨': [120.23, 29.71],
+            '郑州': [113.65, 34.76],
+            '哈尔滨': [126.63, 45.75],
+            '聊城': [115.97, 36.45],
+            '芜湖': [118.38, 31.33],
+            '唐山': [118.02, 39.63],
+            '平顶山': [113.29, 33.75],
+            '邢台': [114.48, 37.05],
+            '德州': [116.29, 37.45],
+            '济宁': [116.59, 35.38],
+            '荆州': [112.239741, 30.335165],
+            '宜昌': [111.3, 30.7],
+            '义乌': [120.06, 29.32],
+            '丽水': [119.92, 28.45],
+            '洛阳': [112.44, 34.7],
+            '秦皇岛': [119.57, 39.95],
+            '株洲': [113.16, 27.83],
+            '石家庄': [114.48, 38.03],
+            '莱芜': [117.67, 36.19],
+            '常德': [111.69, 29.05],
+            '保定': [115.48, 38.85],
+            '湘潭': [112.91, 27.87],
+            '金华': [119.64, 29.12],
+            '岳阳': [113.09, 29.37],
+            '长沙': [113, 28.21],
+            '衢州': [118.88, 28.97],
+            '廊坊': [116.7, 39.53],
+            '菏泽': [115.480656, 35.23375],
+            '合肥': [117.27, 31.86],
+            '武汉': [114.31, 30.52],
+            '大庆': [125.03, 46.58],
+            '鄂州': [114.90, 30.40],
+            '黄石': [115.05, 30.21]
+};
+
+var convertData = function (data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        var geoCoord = geoCoordMap[data[i].name];
+        if (geoCoord) {
+            res.push({
+                name: data[i].name,
+                value: geoCoord.concat(data[i].value),
+                list: eval(data[i].list)
+            });
+        }
+    }
+    return res;
+};
+
+
+var map = echarts.init(document.getElementById('map'));
+var zoom = 1;
+var option = {
+  geo: {
+    map: 'china',
+    label: {
+        emphasis: {
+            show: false
+        }
+    },
+    zoom: 1,
+    roam: true, // 开启鼠标缩放和平移漫游
+    itemStyle: {
+        normal: {
+            areaColor: '#00ce9b',
+            borderColor: '#eee'
+        },
+        emphasis: {
+            areaColor: '#00bc8d'
+        }
+    }
+  },
+  series: [{
+    name: '站点',
+    type: 'effectScatter', // 散点（气泡）图
+    coordinateSystem: 'geo',
+    data: convertData(data),
+    symbolSize: 8,
+    rippleEffect: {
+        brushType: 'stroke'
+    },
+    label: {
+        normal: {
+            formatter: '{b}',
+            position: 'right',
+            show: false
+        },
+        emphasis: {
+            show: false
+        }
+    },
+    itemStyle: {
+        normal: {
+            color: '#00ffff'
+        }
+    }
+  }]
+};
+
+map.setOption(option);
+
+var zoomTime = setInterval(function(){
+	zoomIn(4);
+}, 500);
+
+function zoomIn(i){
+	if(zoom <= i){
+		zoom += 1;
+    option.geo.zoom  = zoom;
+    option.geo.center = [114.31, 30.52];
+		map.setOption(option);
+	}else{
+		setTimeout(zoomTime);
+	}
+}
+
+map.on('click', function (params) {
+    if (params.data) {
+        var x = params.event.event.zrX;
+        var y = params.event.event.zrY;
+        var name = params.data.name;
+        var length = params.data.list.length;
+        $('.pop').remove();
+        var $pop = $('<div class="pop">'+ name +'<br>'+ length +'个站点</div>');
+        $pop.css({left: x - 10, top: y - 81 });
+        $('#map').append($pop);
+    }else {
+      $('.pop').remove();
+    }
+});
